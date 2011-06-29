ea9bf8f0f0537d9b152312b198bae08e49a5a245
Jason Galea <jason@eightdegrees.com.au>
2011-06-29 13:34:56 +1000

    removed unused code

--- lib/PDF/Boxer/Content/Column.pm
+++ lib/PDF/Boxer/Content/Column.pm
@@ -34,16 +34,10 @@ sub update_kids_size{
     my $space = $self->height - $kids_height;
     my ($has_grow,$grow,$grow_all);
     my $space_each = 0;
-#    if ($space > 0){
     foreach my $kid (@$kids){
       $has_grow++ if $kid->grow;
     }
-#      if (!$has_grow){
-#        $grow_all = 1;
-#        $has_grow = @$kids;
-#      }
     $space_each = int($space/$has_grow) if $has_grow;
-#    }
 
     my $kwidth = $self->content_width;
 
@@ -53,7 +47,6 @@ sub update_kids_size{
         $kheight += $space_each;
       }
       $kid->set_margin_size($kwidth, $kheight);
-#      $kid->update;
     }
 
   }
@@ -68,7 +61,6 @@ sub update_kids_position{
     my $left = $self->content_left;
     foreach my $kid (@$kids){
       $kid->move($left, $top);
-#      $kid->update;
       $top -= $kid->margin_height;
     }
   }
@@ -92,137 +84,6 @@ sub child_adjusted_height{
       }
     }
   }
-}
-
-__PACKAGE__->meta->make_immutable;
-
-1;
-
-__END__
-
-
-
-
-sub set_minimum_size{
-  my ($self) = @_;
-
-  my @kids = $self->propagate('set_minimum_size');
-
-  # the main box should stay wide open.
-  return unless $self->parent;
-
-  my ($width, $height);
-  if (@kids){
-    ($width, $height) = $self->kids_min_size;
-  } else {
-    $width = $self->has_width ? $self->width : 0;
-    $height = $self->has_height ? $self->height : 0;
-  }
-
-  $self->adjust({
-     width => $width,
-     height => $height,
-  }, 'self');
-
-  return ($width, $height);
-}
-
-sub size_and_position{
-  my ($self) = @_;
-
-  my ($width, $height) = $self->kids_min_size;
-
-  my $kids = $self->children;
-
-  if (@$kids){
-    my $space = $self->height - $height;
-    my ($has_grow,$grow,$grow_all);
-    my ($space_each);
-    if ($space > 0){
-      foreach my $kid (@$kids){
-        $has_grow++ if $kid->grow;
-      }
-      if (!$has_grow){
-        $grow_all = 1;
-        $has_grow = @$kids;
-      }
-      $space_each = int($space/$has_grow);
-    }
-
-    my $top = $self->content_top;
-    my $left = $self->content_left;
-
-    my $kwidth = $self->content_width;
-
-    foreach my $kid (@$kids){
-      my $kheight = $kid->margin_height;
-      if ($grow_all || $kid->grow){
-        $kheight += $space_each;
-      }
-      $kid->adjust({
-        margin_left => $left,
-        margin_top => $top,
-        margin_width => $kwidth,
-        margin_height => $kheight,
-      },'parent');
-      $top -= $kheight;
-    }
-
-    $self->propagate('size_and_position');
-  }
-
-  return 1;
-}
-
-sub position{
-  my ($self) = @_;
-  my $kids = $self->children;
-  my $top = $self->content_top;
-  my $left = $self->content_left;
-
-  foreach my $kid (@$kids){
-    my $kheight = $kid->margin_height;
-    $kid->adjust({
-      margin_left => $left,
-      margin_top => $top,
-    },'parent');
-    $top -= $kheight;
-  }
-}
-
-sub tighten{
-  my ($self) = @_;
-  $self->propagate('tighten');
-  $self->position;
-
-  my $kid = $self->children->[-1];
-  $self->adjust({
-    content_bottom => $kid->margin_bottom,
-  },'self');
-
-}
-
-
-sub float_kids{
-  my ($self) = @_;
-  my $kids = $self->children;
-
-  if (@$kids){
- 
-
-  }
-
-}
-
-sub kids_min_size{
-  my ($self) = @_;
-  my @kids = @{$self->children};
-  my ($width, $height) = (0,0);
-  foreach(@kids){
-    $height+= $_->margin_height;
-    $width = $width ? (sort { $b <=> $a } ($_->margin_width,$width))[0] : $_->margin_width;
-  }
-  return ($width, $height);
 }
 
 __PACKAGE__->meta->make_immutable;
